54edad51ca5abfaa72229a0611a63f59069973be
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2025-04-07 18:14:19 +0300

    Добавьте файлы проекта.

---
+++ WpfApp20/Pages/Task1Page.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp20.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Task1Page.xaml
+    /// </summary>
+    public partial class Task1Page : Page
+    {
+        public Task1Page()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(dialog.FileName);
+                    double product = 1;
+
+                    foreach (string line in lines)
+                    {
+                        double number;
+                        if (double.TryParse(line, out number))
+                        {
+                            product *= number;
+                        }
+                    }
+
+                    ResultTextBlock.Text = "Произведение: " + product;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
